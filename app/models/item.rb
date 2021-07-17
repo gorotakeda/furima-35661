9fdbf522b0e9be78d6e68b39cfbc6f90469a568b
gorotakeda <rugbytkg3@gmail.com>
2021-07-17 17:50:56 +0900

    商品情報を保存する機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,21 +6,22 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
+
   has_one_attached :image
 
   with_options presence: true do
-  validates :image
-  validates :name
-  validates :info
-  validates :price
+   validates :image
+   validates :name
+   validates :info
+   with_options format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" } do
+     validates :price, inclusion: { in: 300..9999999, message: "is out of setting range"}
+   end
   end
-
-  with_options numericality: { other_than: 0 , message: "can't be blank" } do
-  validates :category_id
-  validates :sales_status_id
-  validates :shipping_fee_status_id
-  validates :prefecture_id
-  validates :scheduled_delivery_id  
-  end
-  
+   with_options numericality: { other_than: 0, message: "can't be blank"} do
+     validates :category_id
+     validates :sales_status_id
+     validates :shipping_fee_status_id
+     validates :prefecture_id
+     validates :scheduled_delivery_id
+   end
 end
