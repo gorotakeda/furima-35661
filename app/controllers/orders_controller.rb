338ef4802da74e8b153e32d3735f9cc4f2760f9a
gorotakeda <rugbytkg3@gmail.com>
2021-07-25 17:01:11 +0900

    ログイン状態のユーザーが、URLを直接入力して売却済み商品の商品購入ページへ遷移しようとすると、トップページに遷移する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
-    redirect_to root_path if current_user == @item.user
+    redirect_to root_path if (current_user == @item.user) || @item.order != nil
   end
 
   def create
